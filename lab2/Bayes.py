aeee9fddef7684299a31da4a9e42c2941bf67215
AntipovDen <antipovden@yandex.ru>
2015-10-17 02:24:24 +0300

    Bayes done without crossvalidation

--- lab2/Bayes.py
+++ lab2/Bayes.py
@@ -3,5 +3,49 @@ __author__ = 'Den'
 from os import listdir
 from os.path import join, isfile
 
-for f in listdir('pu1'):
-    print(isfile(f))
+spam_border = 0.8
+
+words_in_spam = {}
+words_in_ham = {}
+spmsg_count = 0
+hamsg_count = 0
+
+
+# TODO: we should go through all the files from the learning set
+for f in listdir('pu1/part1'):
+    cur = open(join('pu1/part1', f))
+    is_spam = "spmsg" in f
+    words = {int(i) for i in cur.readline().split(' ')[1:]}
+    for i in cur.readlines()[1].split(' '):
+        words.add(int(i))
+    for word in words:
+        if is_spam:
+            spmsg_count += 1
+            if word in words_in_spam:
+                words_in_spam[word] += 1
+            else:
+                words_in_spam[word] = 1
+        else:
+            hamsg_count += 1
+            if word in words_in_ham:
+                words_in_ham[word] += 1
+            else:
+                words_in_ham[word] = 1
+
+word_spaming = {word: words_in_spam[word] / spmsg_count for word in words_in_spam}
+word_haming = {word: words_in_ham[word] / hamsg_count for word in words_in_ham}
+for word in word_spaming:
+    if word not in word_haming:
+        word_haming[word] = 0
+
+# probability that message is spam if it contains a word
+spam_prob = {word_spaming[word] / (word_spaming[word] + word_haming[word]) for word in word_spaming}
+
+def msg_spam_prob(msg):
+    prob_spam, prob_ham = 1., 1.
+    for word in msg:
+        prob_spam *= spam_prob[word]
+        prob_ham *= 1 - spam_prob[word]
+    return prob_spam / (prob_spam + prob_ham)
+
+# TODO: cross-validation should be here
